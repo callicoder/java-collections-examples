c1213e407df71ca5b623fbd7671ed4435f62e86c
Rajeev Kumar Singh <rajeevs@flock.co>
2018-04-24 15:40:44 +0530

    More examples

--- java-treemap-examples/src/AccessEntriesFromTreeMapExample.java
+++ java-treemap-examples/src/AccessEntriesFromTreeMapExample.java
@@ -1,9 +1,10 @@
 import java.util.Map;
 import java.util.TreeMap;
 
-public class AccessRemoveEntriesFromTreeMapExample {
+public class AccessEntriesFromTreeMapExample {
     public static void main(String[] args) {
         TreeMap<Integer, String> employees = new TreeMap<>();
+
         employees.put(1003, "Rajeev");
         employees.put(1001, "James");
         employees.put(1002, "Sachin");
@@ -24,6 +25,10 @@ public class AccessRemoveEntriesFromTreeMapExample {
             System.out.println("Employee does not exist with id : " + id);
         }
 
+        // Find the first and last entry
+        System.out.println("First entry in employees map : " + employees.firstEntry());
+        System.out.println("Last entry in employees map : " + employees.lastEntry());
+
         // Find the entry whose key is just less than the given key
         Map.Entry<Integer, String> employeeJustBelow = employees.lowerEntry(1002);
         System.out.println("Employee just below id 1002 : " + employeeJustBelow);
@@ -31,12 +36,5 @@ public class AccessRemoveEntriesFromTreeMapExample {
         // Find the entry whose key is just higher than the given key
         Map.Entry<Integer, String> employeeJustAbove = employees.higherEntry(1002);
         System.out.println("Employee just above id 1002 : " + employeeJustAbove);
-
-        System.out.println("First entry in employees map : " + employees.firstEntry());
-        System.out.println("Last entry in employees map : " + employees.lastEntry());
-
-        // Removing a key from the TreeMap
-        String name = employees.remove(1003);
-        System.out.println("Removed employee (1003 : " + name + "), New TreeMap " + employees);
     }
 }
---
+++ java-treemap-examples/src/RemoveEntriesFromTreeMapExample.java
@@ -0,0 +1,39 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class RemoveEntriesFromTreeMapExample {
+    public static void main(String[] args) {
+        TreeMap<String, String> countryISOCodeMapping = new TreeMap<>();
+
+        countryISOCodeMapping.put("India", "IN");
+        countryISOCodeMapping.put("United States of America", "US");
+        countryISOCodeMapping.put("China", "CN");
+        countryISOCodeMapping.put("United Kingdom", "UK");
+        countryISOCodeMapping.put("Russia", "RU");
+        countryISOCodeMapping.put("Japan", "JP");
+
+        System.out.println("CountryISOCodeMapping : " + countryISOCodeMapping);
+
+        // Remove the mapping for a given key
+        String countryName = "Japan";
+        String isoCode = countryISOCodeMapping.remove(countryName);
+        if(isoCode != null) {
+            System.out.println("Removed (" + countryName + " => " + isoCode + ") from the TreeMap. New TreeMap " + countryISOCodeMapping);
+        } else {
+            System.out.println(countryName + " does not exist, or it is mapped to a null value");
+        }
+
+        // Remove the mapping for a given key only if it is mapped to a given value
+        countryName = "India";
+        boolean isRemoved = countryISOCodeMapping.remove(countryName, "IA");
+        System.out.println("Was the mapping removed for " + countryName + "? : " + isRemoved);
+
+        // Remove the first entry from the TreeMap
+        Map.Entry<String, String> firstEntry = countryISOCodeMapping.pollFirstEntry();
+        System.out.println("Removed firstEntry : " + firstEntry + ", New TreeMap : " + countryISOCodeMapping);
+
+        // Remove the last entry from the TreeMap
+        Map.Entry<String, String> lastEntry = countryISOCodeMapping.pollLastEntry();
+        System.out.println("Removed lastEntry : " + lastEntry + ", New TreeMap : " + countryISOCodeMapping);
+    }
+}
